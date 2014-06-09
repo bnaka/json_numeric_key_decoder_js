bd41e105a65af537b20c4fb84eb9fc3dc1691c4c
bnaka <bread.sena@gmail.com>
2014-06-09 17:02:41 +0900

    decode関数削除

--- decoder.js
+++ decoder.js
@@ -23,70 +23,6 @@ function pp(v){ if( typeof util === "object" ) { return p(util.inspect(v)); } }
 function pv(n,v){ if( typeof util === "object" ) { return p(n + "=" + util.inspect(v)); } }
 function exit(v){ if( typeof process === "object" ) { process.exit(v); } }
 
-function decode(json_obj, json_key)
-{
-	var undefined;//undefinedの上書き防止
-	p("decode:");
-	pv("\tjson_obj", json_obj);
-	pv("\tjson_key", json_key);
-	if( json_obj === undefined ) { return exit(1); }
-	if( json_key === undefined ) { return exit(1); }
-
-	var obj = {};
-	var num = 0;
-	while( true )
-	{
-		// jsonのkeyは文字列リテラルなので
-		// カウントアップされる数値を文字列に変換
-		var num_s = String(num);
-		// 数値のkeyがjson内にあるか確認 無ければループ終了
-		if( !(num_s in json_obj) ) { break; }
-
-		// json_keyの中に対になるキーが必要
-		if( !(num in json_key) )
-		{
-			p("Error: json_key not found num idx.");
-			pv("num", num);
-			pv("json_key", json_key);
-			return exit(1);
-		}
-
-		// 見通しやすくするため変数に入れる
-		var val = json_obj[num_s]
-		p(num_s + ":" + val);
-
-		// 実際にkeyとなる文字列
-		// 見通しやすくするため変数に入れる
-		var key = json_key[num];
-
-		// 変換対象のオブジェクトにより振り分け
-		if( val instanceof Array )
-		{ // 配列の場合
-			// 配列としてキーを追加
-			obj[key.name] = [];
-			// 配列内のオブジェクトを再帰decodeして追加していく
-			val.forEach(function(v){
-				obj[key.name].push( decode(v, key.array) );
-			});
-			//!@note json的に配列内に配列がある事は無い
-		}
-		else if( val instanceof Object )
-		{ // オブジェクトの場合(ArrayもObjectなので次点で確認)
-			// オブジェクトとしてキーを追加
-			// オブジェクトなので、そのまま再帰decodeする
-			obj[key.name] = decode(val, key.object);
-		}
-		else
-		{
-			// 他のリテラルなのでそのままキーを追加
-			obj[key.name] = val;
-		}
-
-		num++;
-	}
-	return obj;
-}
-
 // 変換用json objのkey名定義クラス
 function JsonObjKey(){
 	this.idx_cnt = 0;
@@ -191,20 +127,6 @@ var json_str = '{ \
 
 var obj = JSON.parse(json_str);
 //pp(obj);
-
-/*
-var a = {};
-
-a.number = obj["0"];
-a.name = obj["1"];
-a.plays = [];
-obj["2"].forEach(function(play){
-	var c = {};
-	c.country = play["0"];
-	a.plays.push(c);
-});
-pp(a);
-*/
 
 // 変換するキーを定義
 var info_key = new JsonObjKey();
